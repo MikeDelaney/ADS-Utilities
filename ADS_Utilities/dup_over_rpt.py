5e85461a6f8ecf12b14a437b740a83b41decc506
Mike Delaney <mdelaney@utexas.edu>
2014-03-14 14:53:49 -0500

    Add classes and tests for duplicate/overlap report records

---
+++ ADS_Utilities/dup_over_rpt.py
@@ -0,0 +1,43 @@
+__author__ = 'michael.delaney'
+
+class DupOverRecord(object):
+    def __init__(self, record=['', '', '', '', '', '', '', '', '', '', '',
+                               '', '', '', '', '', '', '', '', '', '', '',
+                               '', '', '', '', '', '', '', '']):
+        self.rec_num = record[0]
+        self.make_id = record[1]
+        self.make_text = record[2]
+        self.from_year = record[3]
+        self.to_year = record[4]
+        self.model_id = record[5]
+        self.model_text = record[6]
+        self.submodel_id = record[7]
+        self.submodel_text = record[8]
+        self.engine_id = record[9]
+        self.engine_text = record[10]
+        self.attrs = record[11]
+        self.part_group_id = record[12]
+        self.part_group_text = record[13]
+        self.part_descr_id = record[14]
+        self.part_descr_text = record[15]
+        self.part_num = record[16]
+        self.per_car_qty = record[17]
+        self.cat_line_cd = record[18]
+        self.block_cd = record[19]
+        self.block_num = record[20]
+        self.block_seq_num = record[21]
+        self.flag = record[22]
+        self.comment_text = record[23]
+        self.cde2_id = record[24]
+        self.modified_user = record[25]
+        self.modified_date = record[26]
+        self.grouping_id = record[27]
+        self.cde_page_seq_num = record[28]
+        self.filters_and_options = record[29]
+
+class DupOverRecDelete(DupOverRecord):
+    def __init__(self, record=['', '', '', '', '', '', '', '', '', '', '',
+                               '', '', '', '', '', '', '', '', '', '', '',
+                               '', '', '', '', '', '', '', '', '']):
+        self.delete = record.pop()
+        DupOverRecord.__init__(self, record)
---
+++ tests/dup_over_rpt_test.py
@@ -0,0 +1,100 @@
+from nose.tools import *
+from ADS_Utilities.dup_over_rpt import DupOverRecord, DupOverRecDelete
+
+__author__ = 'michael.delaney'
+
+
+class TestDupOverRecord():
+    def setup(self):
+        self.record = DupOverRecord(['1', '1', 'AMC-EAGLE', '1993', '1994',
+                                     '18', 'TALON', '0', '', '0', '',
+                                     '4 WHEEL/ALL WHEEL DRIVE', '28',
+                                     'F BRK HYDRAULICS', '55',
+                                     'Front Brake Hose', '1474-40300', '2',
+                                     'AUN', 'N', '0', '0', '',
+                                     'INNER - TSI - AWD', '16', 'miked',
+                                     '08-OCT-13', '1', '1 0001600', ''])
+
+    def teardown(self):
+        self.record = None
+
+    def test_dupoverrecord_init(self):
+        assert_equal(self.record.rec_num, '1')
+        assert_equal(self.record.make_id, '1')
+        assert_equal(self.record.make_text, 'AMC-EAGLE')
+        assert_equal(self.record.from_year, '1993')
+        assert_equal(self.record.to_year, '1994')
+        assert_equal(self.record.model_id, '18')
+        assert_equal(self.record.model_text, 'TALON')
+        assert_equal(self.record.submodel_id, '0')
+        assert_equal(self.record.submodel_text, '')
+        assert_equal(self.record.engine_id, '0')
+        assert_equal(self.record.engine_text, '')
+        assert_equal(self.record.attrs, '4 WHEEL/ALL WHEEL DRIVE')
+        assert_equal(self.record.part_group_id, '28')
+        assert_equal(self.record.part_group_text, 'F BRK HYDRAULICS')
+        assert_equal(self.record.part_descr_id, '55')
+        assert_equal(self.record.part_descr_text, 'Front Brake Hose')
+        assert_equal(self.record.part_num, '1474-40300')
+        assert_equal(self.record.per_car_qty, '2')
+        assert_equal(self.record.cat_line_cd, 'AUN')
+        assert_equal(self.record.block_cd, 'N')
+        assert_equal(self.record.block_num, '0')
+        assert_equal(self.record.block_seq_num, '0')
+        assert_equal(self.record.flag, '')
+        assert_equal(self.record.comment_text, 'INNER - TSI - AWD')
+        assert_equal(self.record.cde2_id, '16')
+        assert_equal(self.record.modified_user, 'miked')
+        assert_equal(self.record.modified_date, '08-OCT-13')
+        assert_equal(self.record.grouping_id, '1')
+        assert_equal(self.record.cde_page_seq_num, '1 0001600')
+        assert_equal(self.record.filters_and_options, '')
+
+
+class TestDupOverRecDelete():
+    def setup(self):
+        self.record = DupOverRecDelete(['1', '1', 'AMC-EAGLE', '1993', '1994',
+                                        '18', 'TALON', '0', '', '0', '',
+                                        '4 WHEEL/ALL WHEEL DRIVE', '28',
+                                        'F BRK HYDRAULICS', '55',
+                                        'Front Brake Hose', '1474-40300', '2',
+                                        'AUN', 'N', '0', '0', '',
+                                        'INNER - TSI - AWD', '16', 'miked',
+                                        '08-OCT-13', '1', '1 0001600', '',
+                                        'Y'])
+
+    def teardown(self):
+        self.record = None
+
+    def test_dupoverrecdelete_init(self):
+        assert_equal(self.record.rec_num, '1')
+        assert_equal(self.record.make_id, '1')
+        assert_equal(self.record.make_text, 'AMC-EAGLE')
+        assert_equal(self.record.from_year, '1993')
+        assert_equal(self.record.to_year, '1994')
+        assert_equal(self.record.model_id, '18')
+        assert_equal(self.record.model_text, 'TALON')
+        assert_equal(self.record.submodel_id, '0')
+        assert_equal(self.record.submodel_text, '')
+        assert_equal(self.record.engine_id, '0')
+        assert_equal(self.record.engine_text, '')
+        assert_equal(self.record.attrs, '4 WHEEL/ALL WHEEL DRIVE')
+        assert_equal(self.record.part_group_id, '28')
+        assert_equal(self.record.part_group_text, 'F BRK HYDRAULICS')
+        assert_equal(self.record.part_descr_id, '55')
+        assert_equal(self.record.part_descr_text, 'Front Brake Hose')
+        assert_equal(self.record.part_num, '1474-40300')
+        assert_equal(self.record.per_car_qty, '2')
+        assert_equal(self.record.cat_line_cd, 'AUN')
+        assert_equal(self.record.block_cd, 'N')
+        assert_equal(self.record.block_num, '0')
+        assert_equal(self.record.block_seq_num, '0')
+        assert_equal(self.record.flag, '')
+        assert_equal(self.record.comment_text, 'INNER - TSI - AWD')
+        assert_equal(self.record.cde2_id, '16')
+        assert_equal(self.record.modified_user, 'miked')
+        assert_equal(self.record.modified_date, '08-OCT-13')
+        assert_equal(self.record.grouping_id, '1')
+        assert_equal(self.record.cde_page_seq_num, '1 0001600')
+        assert_equal(self.record.filters_and_options, '')
+        assert_equal(self.record.delete, 'Y')
