07760280c188f6363a9ad03709011e0b9914eebd
Mike Delaney <mdelaney@utexas.edu>
2014-03-14 13:05:23 -0500

    Convert tests for PecRec, PecRecSeq to classes with setup/teardown

--- tests/pec_tests.py
+++ tests/pec_tests.py
@@ -18,105 +18,113 @@ def test_specific_condition_str():
     assert_equal(str(spec), ' X168')
 
 
-def test_pecrec_init():
-    record = PecRec('  1 18  0  0  0  0  0  0  0  0  0    0    0    0    0'
-                    '1996199828 55  2AUN1474-41013    N ^C:BASE MODEL - ESI'
-                    ' - w/FRONT DISC BRAKES')
-    assert_equal(record.make, '1')
-    assert_equal(record.model_1, '18')
-    assert_equal(record.model_2, '0')
-    assert_equal(record.model_3, '0')
-    assert_equal(record.model_4, '0')
-    assert_equal(record.model_5, '0')
-    assert_equal(record.model_6, '0')
-    assert_equal(record.engine_1, '0')
-    assert_equal(record.engine_2, '0')
-    assert_equal(record.engine_3, '0')
-    assert_equal(record.engine_4, '0')
-    assert_equal(record.spec_1.or_value, ' ')
-    assert_equal(record.spec_1.x_value, ' ')
-    assert_equal(record.spec_1.code, '0')
-    assert_equal(record.spec_2.or_value, ' ')
-    assert_equal(record.spec_2.x_value, ' ')
-    assert_equal(record.spec_2.code, '0')
-    assert_equal(record.spec_3.or_value, ' ')
-    assert_equal(record.spec_3.x_value, ' ')
-    assert_equal(record.spec_3.code, '0')
-    assert_equal(record.spec_4.or_value, ' ')
-    assert_equal(record.spec_4.x_value, ' ')
-    assert_equal(record.spec_4.code, '0')
-    assert_equal(record.from_year, 1996)
-    assert_equal(record.to_year, 1998)
-    assert_equal(record.group, '28')
-    assert_equal(record.pd, '55')
-    assert_equal(record.pcq, '2')
-    assert_equal(record.lc, 'AUN')
-    assert_equal(record.pn, '1474-41013')
-    assert_equal(record.block_flag, 'N')
-    assert_equal(record.flag, ' ')
-    assert_equal(record.comment, '^C:BASE MODEL - ESI - w/FRONT DISC BRAKES')
+class TestPecRec():
 
-def test_pecrec_str():
-    record = PecRec('  1 18  0  0  0  0  0  0  0  0  0    0    0    0    0'
-                    '1996199828 55  2AUN1474-41013    N ^C:BASE MODEL - ESI'
-                    ' - w/FRONT DISC BRAKES')
-    assert_equal(str(record), '  1 18  0  0  0  0  0  0  0  0  0    0    0   '
-                              ' 0    01996199828 55  2AUN1474-41013    N '
-                              '^C:BASE MODEL - ESI - w/FRONT DISC BRAKES')
+    def setup(self):
+        self.record = PecRec('  1 18  0  0  0  0  0  0  0  0  0    0    0'
+                             '    0    01996199828 55  2AUN1474-41013    N '
+                             '^C:BASE MODEL - ESI - w/FRONT DISC BRAKES')
 
+    def teardown(self):
+        self.record = None
 
-def test_pecrecseq_init():
-    record = PecRecSeq('  1 18  0  0  0  0  0  0  0  0  0    0    0    0    0'
-                       '1996199828 55  2AUN1474-41013    N ^C:BASE MODEL - ESI'
-                       ' - w/FRONT DISC BRAKES^C:***    1/1900     ***')
-    assert_equal(record.make, '1')
-    assert_equal(record.model_1, '18')
-    assert_equal(record.model_2, '0')
-    assert_equal(record.model_3, '0')
-    assert_equal(record.model_4, '0')
-    assert_equal(record.model_5, '0')
-    assert_equal(record.model_6, '0')
-    assert_equal(record.engine_1, '0')
-    assert_equal(record.engine_2, '0')
-    assert_equal(record.engine_3, '0')
-    assert_equal(record.engine_4, '0')
-    assert_equal(record.spec_1.or_value, ' ')
-    assert_equal(record.spec_1.x_value, ' ')
-    assert_equal(record.spec_1.code, '0')
-    assert_equal(record.spec_2.or_value, ' ')
-    assert_equal(record.spec_2.x_value, ' ')
-    assert_equal(record.spec_2.code, '0')
-    assert_equal(record.spec_3.or_value, ' ')
-    assert_equal(record.spec_3.x_value, ' ')
-    assert_equal(record.spec_3.code, '0')
-    assert_equal(record.spec_4.or_value, ' ')
-    assert_equal(record.spec_4.x_value, ' ')
-    assert_equal(record.spec_4.code, '0')
-    assert_equal(record.from_year, 1996)
-    assert_equal(record.to_year, 1998)
-    assert_equal(record.group, '28')
-    assert_equal(record.pd, '55')
-    assert_equal(record.pcq, '2')
-    assert_equal(record.lc, 'AUN')
-    assert_equal(record.pn, '1474-41013')
-    assert_equal(record.block_flag, 'N')
-    assert_equal(record.flag, ' ')
-    assert_equal(record.comment, '^C:BASE MODEL - ESI - w/FRONT DISC BRAKES')
-    assert_equal(record.sequence, '1/1900')
+    def test_pecrec_init(self):
+        assert_equal(self.record.make, '1')
+        assert_equal(self.record.model_1, '18')
+        assert_equal(self.record.model_2, '0')
+        assert_equal(self.record.model_3, '0')
+        assert_equal(self.record.model_4, '0')
+        assert_equal(self.record.model_5, '0')
+        assert_equal(self.record.model_6, '0')
+        assert_equal(self.record.engine_1, '0')
+        assert_equal(self.record.engine_2, '0')
+        assert_equal(self.record.engine_3, '0')
+        assert_equal(self.record.engine_4, '0')
+        assert_equal(self.record.spec_1.or_value, ' ')
+        assert_equal(self.record.spec_1.x_value, ' ')
+        assert_equal(self.record.spec_1.code, '0')
+        assert_equal(self.record.spec_2.or_value, ' ')
+        assert_equal(self.record.spec_2.x_value, ' ')
+        assert_equal(self.record.spec_2.code, '0')
+        assert_equal(self.record.spec_3.or_value, ' ')
+        assert_equal(self.record.spec_3.x_value, ' ')
+        assert_equal(self.record.spec_3.code, '0')
+        assert_equal(self.record.spec_4.or_value, ' ')
+        assert_equal(self.record.spec_4.x_value, ' ')
+        assert_equal(self.record.spec_4.code, '0')
+        assert_equal(self.record.from_year, 1996)
+        assert_equal(self.record.to_year, 1998)
+        assert_equal(self.record.group, '28')
+        assert_equal(self.record.pd, '55')
+        assert_equal(self.record.pcq, '2')
+        assert_equal(self.record.lc, 'AUN')
+        assert_equal(self.record.pn, '1474-41013')
+        assert_equal(self.record.block_flag, 'N')
+        assert_equal(self.record.flag, ' ')
+        assert_equal(self.record.comment, '^C:BASE MODEL - ESI - w/FRONT '
+                                          'DISC BRAKES')
+
+    def test_pecrec_str(self):
+        assert_equal(str(self.record), '  1 18  0  0  0  0  0  0  0  0  0'
+                                       '    0    0    0    01996199828 55  2'
+                                       'AUN1474-41013    N ^C:BASE MODEL - '
+                                       'ESI - w/FRONT DISC BRAKES')
 
 
-def test_format_sequence():
-    record = PecRecSeq('  1 18  0  0  0  0  0  0  0  0  0    0    0    0    0'
-                       '1996199828 55  2AUN1474-41013    N ^C:BASE MODEL - ESI'
-                       ' - w/FRONT DISC BRAKES^C:***    1/1900     ***')
-    assert_equal(record.format_sequence(), '^C:***    1/1900     ***')
+class TestPecRecSeq():
 
+    def setup(self):
+        self.record = PecRecSeq('  1 18  0  0  0  0  0  0  0  0  0    0    0'
+                                '    0    01996199828 55  2AUN1474-41013    N'
+                                ' ^C:BASE MODEL - ESI - w/FRONT DISC BRAKES'
+                                '^C:***    1/1900     ***')
 
-def test_pecrecseq_str():
-    record = PecRecSeq('  1 18  0  0  0  0  0  0  0  0  0    0    0    0    0'
-                       '1996199828 55  2AUN1474-41013    N ^C:BASE MODEL - ESI'
-                       ' - w/FRONT DISC BRAKES^C:***    1/1900     ***')
-    assert_equal(str(record), '  1 18  0  0  0  0  0  0  0  0  0    0    0   '
-                              ' 0    01996199828 55  2AUN1474-41013    N '
-                              '^C:BASE MODEL - ESI - w/FRONT DISC BRAKES'
-                              '^C:***    1/1900     ***')
+    def teardown(self):
+        self.record = None
+
+    def test_pecrecseq_init(self):
+        assert_equal(self.record.make, '1')
+        assert_equal(self.record.model_1, '18')
+        assert_equal(self.record.model_2, '0')
+        assert_equal(self.record.model_3, '0')
+        assert_equal(self.record.model_4, '0')
+        assert_equal(self.record.model_5, '0')
+        assert_equal(self.record.model_6, '0')
+        assert_equal(self.record.engine_1, '0')
+        assert_equal(self.record.engine_2, '0')
+        assert_equal(self.record.engine_3, '0')
+        assert_equal(self.record.engine_4, '0')
+        assert_equal(self.record.spec_1.or_value, ' ')
+        assert_equal(self.record.spec_1.x_value, ' ')
+        assert_equal(self.record.spec_1.code, '0')
+        assert_equal(self.record.spec_2.or_value, ' ')
+        assert_equal(self.record.spec_2.x_value, ' ')
+        assert_equal(self.record.spec_2.code, '0')
+        assert_equal(self.record.spec_3.or_value, ' ')
+        assert_equal(self.record.spec_3.x_value, ' ')
+        assert_equal(self.record.spec_3.code, '0')
+        assert_equal(self.record.spec_4.or_value, ' ')
+        assert_equal(self.record.spec_4.x_value, ' ')
+        assert_equal(self.record.spec_4.code, '0')
+        assert_equal(self.record.from_year, 1996)
+        assert_equal(self.record.to_year, 1998)
+        assert_equal(self.record.group, '28')
+        assert_equal(self.record.pd, '55')
+        assert_equal(self.record.pcq, '2')
+        assert_equal(self.record.lc, 'AUN')
+        assert_equal(self.record.pn, '1474-41013')
+        assert_equal(self.record.block_flag, 'N')
+        assert_equal(self.record.flag, ' ')
+        assert_equal(self.record.comment, '^C:BASE MODEL - ESI - w/FRONT '
+                                          'DISC BRAKES')
+        assert_equal(self.record.sequence, '1/1900')
+
+    def test_format_sequence(self):
+        assert_equal(self.record.format_sequence(), '^C:***    1/1900     ***')
+
+    def test_pecrecseq_str(self):
+        assert_equal(str(self.record), '  1 18  0  0  0  0  0  0  0  0  0'
+                                       '    0    0    0    01996199828 55  2'
+                                       'AUN1474-41013    N ^C:BASE MODEL - '
+                                       'ESI - w/FRONT DISC BRAKES'
+                                       '^C:***    1/1900     ***')
