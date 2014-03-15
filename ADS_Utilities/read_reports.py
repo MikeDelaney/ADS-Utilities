84f5809ac25337ae5d1fe3f7c1825e98b6b52f50
Mike Delaney <mdelaney@utexas.edu>
2014-03-15 08:26:12 -0500

    Add function stubs for reading reports

---
+++ ADS_Utilities/read_reports.py
@@ -0,0 +1,19 @@
+__author__ = 'miked'
+
+
+def load_dupes_overlaps(report_file):
+    """
+    FileObj -> Dict
+    Produces dictionary mapping pec make/sequence number to the corresponding
+    report record
+    """
+    pass
+
+
+def get_column_indexes(header, delete_flag):
+    """
+    listof Str Int -> listof Int
+    Produces list containing indexes of report column names from the report
+    header.  Used for re-ordering columns.
+    """
+    pass
