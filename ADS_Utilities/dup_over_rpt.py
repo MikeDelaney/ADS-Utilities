d122a6f83596d5e43019ed8c7a0626fc5f6cbf6f
Mike Delaney <mdelaney@utexas.edu>
2014-03-16 16:40:38 -0500

    Add __str__ methods to classes

--- ADS_Utilities/dup_over_rpt.py
+++ ADS_Utilities/dup_over_rpt.py
@@ -37,9 +37,28 @@ class DupOverRecord(object):
                            + str(int(record[28][record[28].find(' '):]))
         self.filters_and_options = record[29]
 
+    def __str__(self):
+        return (self.rec_num + ', ' + self.make_id + ', ' + self.make_text +
+                ', ' + self.from_year + ', ' + self.to_year + ', ' +
+                self.model_id + ', ' + self.model_text + ', ' +
+                self.submodel_id + ', ' + self.submodel_text + ', ' +
+                self.engine_id + ', ' + self.engine_text + ', ' + self.attrs +
+                ', ' + self.part_group_id + ', ' + self.part_group_text + ', '
+                + self.part_descr_id + ', ' + self.part_descr_text + ', ' +
+                self.part_num + ', ' + self.per_car_qty + ', ' +
+                self.cat_line_cd + ', ' + self.block_cd + ', ' +
+                self.block_num + ', ' + self.block_seq_num + ', ' + self.flag +
+                ', ' + self.comment_text + ', ' + self.cde2_id + ', ' +
+                self.modified_user + ', ' + self.modified_date + ', ' +
+                self.grouping_id + ', ' + self.cde_page_seq_num + ', ' +
+                self.filters_and_options)
+
 class DupOverRecDelete(DupOverRecord):
     def __init__(self, record=['', '', '', '', '', '', '', '', '', '', '',
                                '', '', '', '', '', '', '', '', '', '', '',
                                '', '', '', '', '', '', '', '', '']):
         self.delete = record.pop()
         DupOverRecord.__init__(self, record)
+
+    def __str__(self):
+        return DupOverRecord.__str__(self) + ', ' + self.delete
