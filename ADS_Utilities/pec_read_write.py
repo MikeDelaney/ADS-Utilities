8da6beef7a3b74a1f5f597474c0a72ce35a13700
Mike Delaney <mdelaney@utexas.edu>
2014-03-17 07:41:58 -0500

    Correct write file function.  Edit error message in load function.

--- ADS_Utilities/pec_read_write.py
+++ ADS_Utilities/pec_read_write.py
@@ -26,7 +26,7 @@ def load_pecfile_dict(pec_file):
                     file_dict[record.sequence] = record
     except KeyError:
         Tk().withdraw()
-        tkMessageBox.showerror('Error', 'Duplicate sequence numbers exist.')
+        tkMessageBox.showerror('Error', 'Duplicate sequence numbers exist in PEC file.')
         sys.exit()
     return file_dict
 
@@ -37,5 +37,5 @@ def write_pecfile_dict(output_file, record_dict):
     Writes pec records from dictionary to output_file
     """
     for key in sorted(record_dict.keys()):
-        output_file.write(str(record_dict[key]))
+        output_file.write('%s\n' % str(record_dict[key]))
     return output_file
