a845ee848fbc3317f2f49a2d1d7157f4cb3fdca1
Mike Delaney <mdelaney@utexas.edu>
2014-03-18 15:00:24 -0500

    Improve tests

--- tests/read_reports_tests.py
+++ tests/read_reports_tests.py
@@ -69,45 +69,45 @@ class TestLoadDupesOverlaps():
         header_delete_reordered.insert(1, header_delete_reordered.pop(28))
         header_delete_reordered.insert(2, header_delete_reordered.pop())
 
-        self.r1_reordered = self.r1[:]
-        self.r1_reordered.insert(1, self.r1_reordered.pop(28))
+        r1_reordered = self.r1[:]
+        r1_reordered.insert(1, r1_reordered.pop(28))
         self.r1_delete = self.r1[:]
         self.r1_delete.append('Y')
-        self.r1_delete_reordered = self.r1_delete[:]
-        self.r1_delete_reordered.insert(1, self.r1_delete_reordered.pop(28))
-        self.r1_delete_reordered.insert(2, self.r1_delete_reordered.pop())
+        r1_delete_reordered = self.r1_delete[:]
+        r1_delete_reordered.insert(1, r1_delete_reordered.pop(28))
+        r1_delete_reordered.insert(2, r1_delete_reordered.pop())
 
-        self.r2_reordered = self.r2[:]
-        self.r2_reordered.insert(1, self.r2_reordered.pop(28))
+        r2_reordered = self.r2[:]
+        r2_reordered.insert(1, r2_reordered.pop(28))
         self.r2_delete = self.r2[:]
         self.r2_delete.append('')
-        self.r2_delete_reordered = self.r2_delete[:]
-        self.r2_delete_reordered.insert(1, self.r2_delete_reordered.pop(28))
-        self.r2_delete_reordered.insert(2, self.r2_delete_reordered.pop())
+        r2_delete_reordered = self.r2_delete[:]
+        r2_delete_reordered.insert(1, r2_delete_reordered.pop(28))
+        r2_delete_reordered.insert(2, r2_delete_reordered.pop())
 
-        self.r3_reordered = self.r3[:]
-        self.r3_reordered.insert(1, self.r3_reordered.pop(28))
+        r3_reordered = self.r3[:]
+        r3_reordered.insert(1, r3_reordered.pop(28))
         self.r3_delete = self.r3[:]
         self.r3_delete.append('')
-        self.r3_delete_reordered = self.r3_delete[:]
-        self.r3_delete_reordered.insert(1, self.r3_delete_reordered.pop(28))
-        self.r3_delete_reordered.insert(2, self.r3_delete_reordered.pop())
+        r3_delete_reordered = self.r3_delete[:]
+        r3_delete_reordered.insert(1, r3_delete_reordered.pop(28))
+        r3_delete_reordered.insert(2, r3_delete_reordered.pop())
 
-        self.r4_reordered = self.r4[:]
-        self.r4_reordered.insert(1, self.r4_reordered.pop(28))
+        r4_reordered = self.r4[:]
+        r4_reordered.insert(1, r4_reordered.pop(28))
         self.r4_delete = self.r4[:]
         self.r4_delete.append('Y')
-        self.r4_delete_reordered = self.r4_delete[:]
-        self.r4_delete_reordered.insert(1, self.r4_delete_reordered.pop(28))
-        self.r4_delete_reordered.insert(2, self.r4_delete_reordered.pop())
+        r4_delete_reordered = self.r4_delete[:]
+        r4_delete_reordered.insert(1, r4_delete_reordered.pop(28))
+        r4_delete_reordered.insert(2, r4_delete_reordered.pop())
 
-        self.r5_reordered = self.r5[:]
-        self.r5_reordered.insert(1, self.r5_reordered.pop(28))
+        r5_reordered = self.r5[:]
+        r5_reordered.insert(1, r5_reordered.pop(28))
         self.r5_delete = self.r5[:]
         self.r5_delete.append('')
-        self.r5_delete_reordered = self.r5_delete[:]
-        self.r5_delete_reordered.insert(1, self.r5_delete_reordered.pop(28))
-        self.r5_delete_reordered.insert(2, self.r5_delete_reordered.pop())
+        r5_delete_reordered = self.r5_delete[:]
+        r5_delete_reordered.insert(1, r5_delete_reordered.pop(28))
+        r5_delete_reordered.insert(2, r5_delete_reordered.pop())
 
         separator_delete = separator[:]
         separator_delete.append('')
@@ -128,12 +128,12 @@ class TestLoadDupesOverlaps():
                                           '\t'.join(self.r5) + '\n' +
                                           '\t'.join(filter_rec) + '\n')
         self.report_2 = StringIO.StringIO('\t'.join(header_reordered) + '\n' +
-                                          '\t'.join(self.r1_reordered) + '\n' +
-                                          '\t'.join(self.r2_reordered) + '\n' +
+                                          '\t'.join(r1_reordered) + '\n' +
+                                          '\t'.join(r2_reordered) + '\n' +
                                           '\t'.join(separator) + '\n' +
-                                          '\t'.join(self.r3_reordered) + '\n' +
-                                          '\t'.join(self.r4_reordered) + '\n' +
-                                          '\t'.join(self.r5_reordered) + '\n' +
+                                          '\t'.join(r3_reordered) + '\n' +
+                                          '\t'.join(r4_reordered) + '\n' +
+                                          '\t'.join(r5_reordered) + '\n' +
                                           '\t'.join(filter_rec) + '\n')
         self.report_3 = StringIO.StringIO('\t'.join(header_delete) + '\n' +
                                           '\t'.join(self.r1_delete) + '\n' +
@@ -145,17 +145,17 @@ class TestLoadDupesOverlaps():
                                           '\t'.join(filter_rec_delete) + '\n')
         self.report_4 = StringIO.StringIO('\t'.join(header_delete_reordered) +
                                           '\n' +
-                                          '\t'.join(self.r1_delete_reordered) +
+                                          '\t'.join(r1_delete_reordered) +
                                           '\n' +
-                                          '\t'.join(self.r2_delete_reordered) +
+                                          '\t'.join(r2_delete_reordered) +
                                           '\n' +
                                           '\t'.join(separator_delete_reordered)
                                           + '\n' +
-                                          '\t'.join(self.r3_delete_reordered) +
+                                          '\t'.join(r3_delete_reordered) +
                                           '\n' +
-                                          '\t'.join(self.r4_delete_reordered) +
+                                          '\t'.join(r4_delete_reordered) +
                                           '\n' +
-                                          '\t'.join(self.r5_delete_reordered) +
+                                          '\t'.join(r5_delete_reordered) +
                                           '\n' +
                                           '\t'.join(filter_rec_delete_reordered)
                                           + '\n')
@@ -170,57 +170,43 @@ class TestLoadDupesOverlaps():
         self.r3 = None
         self.r4 = None
         self.r5 = None
-        self.r1_reordered = None
-        self.r2_reordered = None
-        self.r3_reordered = None
-        self.r4_reordered = None
-        self.r5_reordered = None
         self.r1_delete = None
         self.r2_delete = None
         self.r3_delete = None
         self.r4_delete = None
         self.r5_delete = None
-        self.r1_delete_reordered = None
-        self.r2_delete_reordered = None
-        self.r3_delete_reordered = None
-        self.r4_delete_reordered = None
-        self.r5_delete_reordered = None
 
-    def test_load_dupes_overlaps_size(self):
-        assert_equal(len(load_dupes_overlaps(self.report_1)), 4)
-        assert_equal(len(load_dupes_overlaps(self.report_2)), 4)
-        assert_equal(len(load_dupes_overlaps(self.report_3)), 4)
-        assert_equal(len(load_dupes_overlaps(self.report_4)), 4)
+    def test_load_default(self):
+        assert_items_equal(load_dupes_overlaps(self.report_1),
+                           {'6/2000': DupOverRecord(self.r1),
+                            '6/2200': DupOverRecord(self.r2),
+                            '6/5600': DupOverRecord(self.r3),
+                            '6/5700': [DupOverRecord(self.r4),
+                                       DupOverRecord(self.r5)]})
 
-    def test_load_dupes_overlaps_values(self):
-        report_dict_1 = load_dupes_overlaps(self.report_1)
-        report_dict_2 = load_dupes_overlaps(self.report_2)
-        report_dict_3 = load_dupes_overlaps(self.report_3)
-        report_dict_4 = load_dupes_overlaps(self.report_4)
+    def test_load_default_reordered(self):
+        assert_items_equal(load_dupes_overlaps(self.report_2),
+                           {'6/2000': DupOverRecord(self.r1),
+                            '6/2200': DupOverRecord(self.r2),
+                            '6/5600': DupOverRecord(self.r3),
+                            '6/5700': [DupOverRecord(self.r4),
+                                       DupOverRecord(self.r5)]})
 
-        assert_equal(str(report_dict_1['6/2000'][0]), str(DupOverRecord(self.r1)))
-        assert_equal(str(report_dict_1['6/2200'][0]), str(DupOverRecord(self.r2)))
-        assert_equal(str(report_dict_1['6/5600'][0]), str(DupOverRecord(self.r3)))
-        assert_equal(str(report_dict_1['6/5700'][0]), str(DupOverRecord(self.r4)))
-        assert_equal(str(report_dict_1['6/5700'][1]), str(DupOverRecord(self.r5)))
+    def test_load_delete(self):
+        assert_items_equal(load_dupes_overlaps(self.report_3),
+                           {'6/2000': DupOverRecDelete(self.r1_delete),
+                            '6/2200': DupOverRecDelete(self.r2_delete),
+                            '6/5600': DupOverRecDelete(self.r3_delete),
+                            '6/5700': [DupOverRecDelete(self.r4_delete),
+                                       DupOverRecDelete(self.r5_delete)]})
 
-        assert_equal(str(report_dict_2['6/2000'][0]), str(DupOverRecord(self.r1)))
-        assert_equal(str(report_dict_2['6/2200'][0]), str(DupOverRecord(self.r2)))
-        assert_equal(str(report_dict_2['6/5600'][0]), str(DupOverRecord(self.r3)))
-        assert_equal(str(report_dict_2['6/5700'][0]), str(DupOverRecord(self.r4)))
-        assert_equal(str(report_dict_2['6/5700'][1]), str(DupOverRecord(self.r5)))
-
-        assert_equal(str(report_dict_3['6/2000'][0]), str(DupOverRecDelete(self.r1_delete)))
-        assert_equal(str(report_dict_3['6/2200'][0]), str(DupOverRecDelete(self.r2_delete)))
-        assert_equal(str(report_dict_3['6/5600'][0]), str(DupOverRecDelete(self.r3_delete)))
-        assert_equal(str(report_dict_3['6/5700'][0]), str(DupOverRecDelete(self.r4_delete)))
-        assert_equal(str(report_dict_3['6/5700'][1]), str(DupOverRecDelete(self.r5_delete)))
-
-        assert_equal(str(report_dict_4['6/2000'][0]), str(DupOverRecDelete(self.r1_delete)))
-        assert_equal(str(report_dict_4['6/2200'][0]), str(DupOverRecDelete(self.r2_delete)))
-        assert_equal(str(report_dict_4['6/5600'][0]), str(DupOverRecDelete(self.r3_delete)))
-        assert_equal(str(report_dict_4['6/5700'][0]), str(DupOverRecDelete(self.r4_delete)))
-        assert_equal(str(report_dict_4['6/5700'][1]), str(DupOverRecDelete(self.r5_delete)))
+    def test_load_delete_reordered(self):
+        assert_items_equal(load_dupes_overlaps(self.report_4),
+                           {'6/2000': DupOverRecDelete(self.r1_delete),
+                            '6/2200': DupOverRecDelete(self.r2_delete),
+                            '6/5600': DupOverRecDelete(self.r3_delete),
+                            '6/5700': [DupOverRecDelete(self.r4_delete),
+                                       DupOverRecDelete(self.r5_delete)]})
 
 
 class TestGetColumnIndexes():
@@ -288,13 +274,19 @@ class TestGetColumnIndexes():
         self.header_default_reordered = None
         self.header_default_delete_reordered = None
 
-    def test_get_column_indexes(self):
+    def test_get_column_indexes_default(self):
         assert_equal(get_column_indexes(self.header_default, False), range(30))
+
+    def test_get_column_indexes_default_reordered(self):
         assert_equal(get_column_indexes(self.header_default_reordered, False),
                      [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
                       17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 1, 29])
+
+    def test_get_column_indexes_delete(self):
         assert_equal(get_column_indexes(self.header_default_delete, True),
                      range(31))
+
+    def test_get_column_indexes_del_reordered(self):
         assert_equal(get_column_indexes(self.header_default_delete_reordered,
                                         True),
                      [0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
