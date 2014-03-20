b132450fd13d78509c72633e3c23483f9c479234
Mike Delaney <mdelaney@utexas.edu>
2014-03-20 09:07:10 -0500

    Fix spacing

--- tests/get_specs_tests.py
+++ tests/get_specs_tests.py
@@ -4,6 +4,7 @@ import mock
 
 __author__ = 'michael.delaney'
 
+
 def test_spec_to_val():
     ADS_Utilities.get_specs.get_rows = mock.Mock(return_value=[(u'1 BBL', 1),
                                                                (u'2 BBL', 2)])
